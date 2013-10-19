979629e509de6f2e8bbf71a5ff2cb71f0232bb0d
Calvin Froedge <calvinfroedge@gmail.com>
2013-10-19 09:19:46 -0500

    Working on vote results

--- api/votes.js
+++ api/votes.js
@@ -1,9 +1,10 @@
 module.exports = function(sio, mongoObject){
 	return {
 		vote: function(req, res){
-			sio.sockets.emit('vote cast', req.params.id);
-			castVote(req.params.id);
-			res.send('ok');
+			castVote(req.params.id, function(){
+				sio.sockets.emit('vote cast', req.params.id);
+				res.send('ok');
+			});
 		},
 		results: function(req, res) {
       countVotes(function(results){
@@ -34,7 +35,7 @@ module.exports = function(sio, mongoObject){
 
   }
 
-  function castVote(id) {
+  function castVote(id, success) {
 
     var MongoClient = mongoObject.client;
     var format = mongoObject.format;
@@ -53,16 +54,9 @@ module.exports = function(sio, mongoObject){
       
       collection.insert(data, function(err, docs) {
 
-        collection.count(function(err, count) {
-          console.log(format("count = %s", count));
-        });
-
-        // Locate all the entries using find
-        collection.find().toArray(function(err, results) {
-          console.dir(results);
-          // Let's close the db
-          db.close();
-        });      
+	      if(!err){
+	        success();
+	      }
       });
 
     });
--- app.js
+++ app.js
@@ -4,6 +4,8 @@
  */
 
 var express = require('express');
+var parseCookie = express.CookieParser();
+var MemoryStore = express.session.MemoryStore;
 var http = require('http');
 var path = require('path');
 var app = express();
@@ -13,7 +15,7 @@ var winston = require('winston');
 //var MongoDB = require('winston-mongodb').MongoDB;
 //winston.add(MongoDB, {db:'hacknashville', safe:false});
 //winston.add(winston.transports.Console);
-//MongoStore = require('connect-mongo')(express),
+var MongoStore = require('connect-mongo')(express);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,24 +27,43 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+//Mongo
+// var MongoClient = require('mongodb').MongoClient;
+// var format = require('util').format;
+// var database = 'mongodb://127.0.0.1:27017/test';
+var dbName = 'test';
+var mongoObject = {
+  'client': require('mongodb').MongoClient,
+  'format': require('util').format,
+  'database': 'mongodb://127.0.0.1:27017/'+dbName,
+  'collection_name': 'test_insert'
+};
+
+app.use(express.session({
+	secret: 'foobarbaz',
+	store: new MongoStore({
+		db: dbName
+	}),
+	key: 'express.sid'
+}));
 
 var sio = io.listen(9001);
+
+/*sio.set('authorization', function(data, accept){
+	console.log(data);
+	if(data.headers.cookie){
+		//data.cookie = parseCookie(data.headers.cookie);
+		//data.sessionID = data.cookie['express.sid'];
+	}	else {
+		//return accept('No cookie transmitted', false);
+	}
+	accept(null, true);
+});*/
 
 sio.sockets.on('connection', function(socket){
 	console.log('a socket has connected');
 	socket.emit('foo', 'bar');
 });
-
-//Mongo
-// var MongoClient = require('mongodb').MongoClient;
-// var format = require('util').format;
-// var database = 'mongodb://127.0.0.1:27017/test';
-var mongoObject = {
-  'client': require('mongodb').MongoClient,
-  'format': require('util').format,
-  'database': 'mongodb://127.0.0.1:27017/test',
-  'collection_name': 'test_insert'
-};
 
 var items = require('./api/items');
 var votes = require('./api/votes')(sio, mongoObject);
