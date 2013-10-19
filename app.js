98107a66fabc225b11a4d2193daaee284aaa1a47
Hakan Tunc <hakan.u.tunc@outlook.com>
2013-10-19 16:19:48 -0500

    rest api for post new voteable:/api/items/new

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ if(err){
 
 function routes(sio, db, config) {
 	
-	var items = require('./api/items')(config);
+	var items = require('./api/items')(sio, db, config);
 	var votes = require('./api/votes')(sio, db, config);
 	
 	app.get('/', function(req, res){
@@ -66,6 +66,8 @@ function routes(sio, db, config) {
 		req.session.voted = true;
 		res.send('ok');
 	});
+
+  app.post('/api/items/new', items.add);
 	
 	app.get('/api/items', items.list);
 	app.post('/api/vote/:id', votes.vote);
