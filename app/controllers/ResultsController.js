0781c2fa3c9ec7a08e68ae5d1440b6bf8025eb96
Calvin Froedge <calvinfroedge@gmail.com>
2013-10-19 04:32:17 -0500

    Basic chart integrated

--- app/controllers/ResultsController.js
+++ app/controllers/ResultsController.js
@@ -1,5 +1,33 @@
 Application.main.controller('ResultsController', ['$scope', '$http', ResultsController]);
 function ResultsController ( $scope, $http ) {
+	function draw(){
+		var el = document.getElementById('chart');
+		var ael = angular.element(el)[0];
+		var width = ael.offsetParent.clientWidth;
+		var height = ael.offsetParent.clientHeight;
+		ael.setAttribute('width', width);
+		ael.setAttribute('height', height);
+		console.log(angular.element(el));
+		var ctx = el.getContext('2d');
+
+		var data = {
+			labels : ["foo", "bar"],
+			datasets : [
+				{
+					fillColor : "rgba(151,187,205,0.5)",
+					strokeColor : "rgba(151,187,205,1)",
+					data : [25,48]
+				}
+			]
+		}
+
+		var opts = {
+			scaleShowGridLines: false
+		}
+
+		new Chart(ctx).Bar(data, opts);
+	}
+
 	$http.get('/api/results')
 		.success(function(data){
 			console.log(data);
@@ -8,4 +36,5 @@ function ResultsController ( $scope, $http ) {
 			console.log(data);
 		});
 	$scope.model = {};
+	draw();
 }
