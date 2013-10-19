5eaf7b677acf6050808e190c171ce9cf041e4519
Calvin Froedge <calvinfroedge@gmail.com>
2013-10-19 15:23:03 -0500

    vote stuff

--- app/config.js
+++ app/config.js
@@ -1,9 +1,16 @@
 Application.config = {
-	logo: 'images/logo_hn4.png'
+	logo: 'images/logo_hn4.png',
+	votes: 0
 }
 
 angular.module('application.main')
 
-.factory('Config', function(){
+.factory('Config', function($http, $rootScope){
+	$http.get('/api/info')
+		.success(function(data){
+			Application.config.votes = data.votes;
+			$rootScope.$broadcast('numVotesLoaded', data.votes);
+		})
+		.error();
 	return Application.config;
 })
