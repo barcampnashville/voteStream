d98394bc752e856ea39a38614b459e54b80e4c28
benstucki <benstucki@gmail.com>
2013-10-19 11:52:28 -0500

    moving to persistent db connection to avoid wrapping code. can refactor to util later

--- api/votes.js
+++ api/votes.js
@@ -1,4 +1,4 @@
-module.exports = function(sio, mongoObject){
+module.exports = function(sio, db){
 	return {
 		vote: function(req, res){
 			castVote(req.params.id, function(){
@@ -14,54 +14,20 @@ module.exports = function(sio, mongoObject){
 	}
 
   function countVotes(success) {
-    
-    var MongoClient = mongoObject.client;
-    var format = mongoObject.format;
-    var database = mongoObject.database;
-    var collection_name = mongoObject.collection_name;
-
-    MongoClient.connect(database, function(err, db) {
-      if(err){
-	      console.log('there was an error!', err, db);
-	      success('foo');
-      } else {
-				var collection = db.collection(collection_name);
-
-				collection.aggregate( [ {$group: { _id: '$vote', count: { $sum: 1 } } }], function(err, rsl) {
-					console.log(rsl);
-					success(rsl);
-				});
-      }
-    });
-
+		var collection = db.collection('test_insert');
+		collection.aggregate( [ {$group: { _id: '$vote', count: { $sum: 1 } } }], function(err, rsl) {
+			console.log(rsl);
+			success(rsl);
+		});
   }
 
   function castVote(id, success) {
-
-    var MongoClient = mongoObject.client;
-    var format = mongoObject.format;
-    var database = mongoObject.database;
-    var collection_name = mongoObject.collection_name;
-
-    MongoClient.connect(database, function(err, db) {
-
-      if(err) throw err;
-
-      var collection = db.collection(collection_name);
-
-      var data = {
-        vote: id
-      };
-      
-      collection.insert(data, function(err, docs) {
-
-	      if(!err){
-	        success();
-	      }
-      });
-
-	    db.close();
-
+		var collection = db.collection('test_insert');
+    var data = {vote: id};
+    collection.insert(data, function(err, docs) {
+			if(!err){
+	      success();
+	    }
     });
   }
 
