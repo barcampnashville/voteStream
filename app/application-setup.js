c9448a2d8c1915e87f265704664f7dc1af4ae6fa
Blake McCool <blake.mccool@gmail.com>
2013-10-30 15:43:26 -0500

    removed more unused files; adjusted templates

--- app/application-setup.js
+++
@@ -1,7 +0,0 @@
-var Application = window.Application = window.Application || {};
-Application.main = angular.module('application.main', [])
-
-.factory('Sockets', function(){
-	return io.connect('ws://192.168.14.66:9001');
-});
-//Application.Resolvers = {};
