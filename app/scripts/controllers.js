00f1d328303a156d3c0d20d68214641ca20ef837
Blake McCool <blake.mccool@gmail.com>
2013-10-31 22:41:04 -0500

    adjusted sign in page;

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,35 +5,50 @@
 
 	app.controller({
 
-		SigninController: function ($scope) {
-			$scope.user = {
-				email: 'user@barcamp.com',
-				badgeId: ''
-			};
+		BarcampController: function ($scope, angularFireAuth) {
+			var barcampRef = $scope.barcampRef = new Firebase('https//barcamp.firebaseio.com/');
+			angularFireAuth.initialize(barcampRef, {scope: $scope, name: 'user'});
+			/*var auth = new FirebaseSimpleLogin(barcampRef, function (error, user) {
+				if (error) {
+					console.log(error);
+				} else if (user) {
+					console.log(user);
+				}
+			});*/
+		},
 
-			$scope.submitDetails = function () {};
+		SigninController: function ($scope, angularFireAuth) {
+
+			$scope.submitDetails = function (id) {
+				$scope.barcampRef.child('Users');
+				angularFireAuth.login('anonymous');
+				console.log($scope.user);
+			};
 		},
 
 		ResultsController: function($scope, angularFire) {
 
 			var ref = new Firebase('https://barcamp.firebaseio.com/Sessions');
-			$scope.sessions = angularFire(ref, $scope, 'sessions'); 
-			$scope.gridOptions = { 
-					data: 'sessions' ,
-					enableCellSelection: true,
-					enableRowSelection: false,
-					enableCellEditOnFocus: true,
-					sortBy: 'total_votes',
-					columnDefs: [{field: 'id', displayName: 'ID', enableCellEdit: false, width: '10%', resizable: true}, 
-								{field: 'Title', displayName: 'Title', enableCellEdit: false, width: '40%', resizable: true},
-								{field: 'Username', displayName: 'Username', enableCellEdit: true, width: '20%', resizable: true},
-								{field:'Room', displayName:'Room', enableCellEdit: true, width: '10%', resizable: true},
-								{field:'Time', displayName:'Time', enableCellEdit: true, width: '10%', resizable: true},
-								{field:'total_votes', displayName:'Votes', enableCellEdit: true, width: '10%', resizable: true}]
-					};
+			$scope.sessions = angularFire(ref, $scope, 'sessions');
+			$scope.gridOptions = {
+				data: 'sessions',
+				enableCellSelection: true,
+				enableRowSelection: false,
+				enableCellEditOnFocus: true,
+				sortBy: 'total_votes',
+				columnDefs: [
+					{field: 'id', displayName: 'ID', enableCellEdit: false, width: '10%', resizable: true},
+					{field: 'Title', displayName: 'Title', enableCellEdit: false, width: '40%', resizable: true},
+					{field: 'Username', displayName: 'Username', enableCellEdit: true, width: '20%', resizable: true},
+					{field:'Room', displayName:'Room', enableCellEdit: true, width: '10%', resizable: true},
+					{field:'Time', displayName:'Time', enableCellEdit: true, width: '10%', resizable: true},
+					{field:'total_votes', displayName:'Votes', enableCellEdit: true, width: '10%', resizable: true}
+				]
+			};
 		},
 
-		SessionListingController: function ($scope) {
+		SessionListingController: function ($scope, $location) {
+
 			$scope.votesRemaining = 4;
 			$scope.mysessionlist = [];
 
