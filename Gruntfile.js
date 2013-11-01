e345a007c920d1ddec22b02b9c6285612a5a76e8
Bill Butler <bill@butler.net>
2013-11-01 16:44:34 -0500

    Procfile for heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-              port: 9000,
+              port: 80,
               livereload: 35729,
               // change this to '0.0.0.0' to access the server from outside
               hostname: '0.0.0.0',
