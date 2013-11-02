b3f2aed117aa47e649e9c301e1a47768ca4956f9
Blake McCool <blake.mccool@gmail.com>
2013-11-01 20:05:01 -0500

    another try

--- Gruntfile.js
+++ Gruntfile.js
@@ -306,7 +306,7 @@ module.exports = function (grunt) {
         'build'
     ]);
 
-    grunt.registerTask('heroku:production', 'build');
+    grunt.registerTask('heroku', 'build');
 
     //grunt.registerTask('heroku', 'server');
 };
