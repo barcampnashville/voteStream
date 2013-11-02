17609412f5c840a69591c4cc1df25a3f0b28a135
Blake McCool <blake.mccool@gmail.com>
2013-11-01 22:32:37 -0500

    clean out SigninController

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,60 +7,7 @@
 
 		BarcampController: function ($scope, angularFireAuth) {},
 
-		SigninController: function ($scope, AuthService) {
-			var ref = new Firebase('https://barcamp.firebaseio.com/');
-
-			$scope.authCallback = function (error, user) {
-				if (error) {
-					console.log('error: ' + error.code);
-				} else if (user) {
-					localStorage.setItem('token', user.firebaseAuthToken);
-					$scope.isLoggedIn = true;
-					$scope.userid = user.id;
-
-					$scope.userRef = ref.child('Users').child(id);
-					$scope.userRef.once('value', function (data) {
-						var val = data.val();
-						var info = {
-							userId: user.id,
-							valid: data.valid
-						};
-						if (val) {
-							info = val;
-						}
-						$scope.userRef.set(info);
-
-						$location.path('/sessions');
-					});
-				} else {
-					localStorage.clear();
-					$scope.isLoggedIn = false;
-					$location.path('/');
-				}
-			};
-
-			$scope.login = function(provider) {
-				$scope.token = localStorage.getItem('token');
-				var options = {
-					'rememberMe': true
-				};
-				provider = 'anonymous';
-
-				if ($scope.token) {
-					console.log('login with token', $scope.token);
-					fireFactory.firebaseRef('users').auth($scope.token, $scope.authCallback);
-				} else {
-					console.log('login with authClient');
-					authClient.login(provider, options);
-				}
-			};
-
-			$scope.logout = function() {
-				localStorage.clear();
-				authClient.logout();
-				$location.path('/');
-			};
-		},
+		SigninController: function ($scope, AuthService) {},
 
 		ResultsController: function($scope, angularFire) {
 
