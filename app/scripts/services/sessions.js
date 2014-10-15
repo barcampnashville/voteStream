5cae6011f60b81998aa9dd4bee7d0e4fb40040e5
Blake McCool <blake.mccool@gmail.com>
2014-10-14 20:16:08 -0500

    make use of session service

--- app/scripts/services/sessions.js
+++ app/scripts/services/sessions.js
@@ -1,19 +1,33 @@
 angular.module('BarcampApp')
 	.factory('Session', function ($firebase) {
-		var Session = function (session, id) {
+		var Session = function (session) {
+			var id = session.id;
 			this.ref = new Firebase('https://barcamp.firebaseio.com/Sessions/' + id);
-			this.sessObj = $firebase(this.ref).$asObject();
+			this.id = session.id;
+			this.availability = session.Availability;
+			this.summary = session.Body;
+			this.speaker = {
+					firstName: session['First Name'],
+					lastName: session['Last Name'],
+					email: session['E-mail']
+			};
+			this.categories = session['Session Category'];
+			this.totalSignUps = session['Signup Counts'];
+			this.time = session.Time;
+			this.title = session.Title;
+			this.hashtag = session['Twitter Hashtag'];
+			this.totalVotes = session.total_votes;
 		};
 
-		Session.prototype.vote = function (val) {
-			this.voteRef.transaction(function (current) {
+		Session.prototype.vote = function () {
+			/*this.voteRef.transaction(function (current) {
 				return current + val;
-			});
+			});*/
 		};
 
 		return Session;
 	})
-	.service('SessionListing', function ($firebase, $q, NormalizeSession) {
+	.service('SessionListing', function ($firebase, $q, Session) {
 		var defer = $q.defer();
 		var items = [];
 		if (items.length > 0) {
@@ -22,30 +36,10 @@ angular.module('BarcampApp')
 		var list = $firebase(new Firebase('https://barcamp.firebaseio.com/Sessions')).$asArray();
 		list.$loaded().then(function (sessions) {
 			sessions.forEach(function (sess) {
-				items.push(NormalizeSession(sess));
+				items.push(new Session(sess));
 			});
 			defer.resolve(items);
 		});
 		return defer.promise;
-	})
-	.factory('NormalizeSession', function () {
-		return function (session) {
-			return {
-				id: session.id,
-				availability: session.Availability,
-				summary: session.Body,
-				speaker: {
-					firstName: session['First Name'],
-					lastName: session['Last Name'],
-					email: session['E-mail']
-				},
-				categories: session['Session Category'],
-				totalSignUps: session['Signup Counts'],
-				time: session.Time,
-				title: session.Title,
-				hashtag: session['Twitter Hashtag'],
-				totalVotes: session.total_votes
-			}
-		}
 	})
 ;
