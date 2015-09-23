ba823acdf5028a6238f1722b736177589f2cd461
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-09-23 14:54:02 -0500

    deletes switcher.js

--- app/scripts/controllers/switcher.js
+++
@@ -1,6 +0,0 @@
-//switch code
-angular.module('BarcampApp', ['ngAnimate'])
-.controller('SwitchCtrl', ['$scope', function($scope){
-  $scope.items =  ['morning', 'afternoon'];
-  $scope.selection = $scope.items[0];
-}]);
