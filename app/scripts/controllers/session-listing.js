fb4a3cdeb8eb657a9e98490ac2df67cd2fb97a25
Paul Sunderhaus <sunderhauspaul@gmail.com>
2015-10-12 22:40:49 -0500

    Correcting voting options to be driven off admin settings

--- app/scripts/controllers/session-listing.js
+++ app/scripts/controllers/session-listing.js
@@ -7,7 +7,7 @@ angular.module('BarcampApp')
 		$rootScope.pollingSync.$watch(function () {
 			$scope.polling = $rootScope.pollingSync;
 		});
-		
+
 		$rootScope.user.ref.once('value', function (snapshot) {
 			if (snapshot.hasChild('sessions')) {
 				var s = snapshot.val().sessions;
@@ -16,21 +16,14 @@ angular.module('BarcampApp')
 				}
 			}
 		});
-        var favorites = [];
-        var favoritesFromUser = [];
-          $rootScope.user.ref.once('value', function (snapshot){
-             if (snapshot.hasChild('favorites')) {
-               $scope.userHasFavorites = true;
-               favoritesFromUser = snapshot.favorites.val();
-             }
 
-          });
-  /*$rootScope.Sessions.ref.once('value', function (snapshot){
-    console.log(snapshot);
-  })*/ console.log($rootScope);
-
-
-})
-;
+    $scope.usersFavorites = favoritesFromUser = [];
+    $rootScope.user.ref.once('value', function (snapshot){
+      if (snapshot.hasChild('favorites')) {
+        $scope.userHasFavorites = true;
+        favoritesFromUser = snapshot.favorites.val();
+      }
+    });
+});
 
 //need to write a method that takes the information from the sessions and changes them based on whether someone favorited them and return that array as a view in the sessionlist.html
