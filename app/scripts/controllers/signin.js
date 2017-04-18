ce1e5296c23e1e1b879bbd8280362fef15ee9247
Banks Humphreys <iobg0320@gmail.com>
2017-04-18 18:56:56 -0500

    retrieving badge id's with GET request

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -1,11 +1,11 @@
 angular.module('BarcampApp')
-    .controller('SigninCtrl', function ($scope, $location) {
+    .controller('SigninCtrl', function ($scope, $location,$http) {
         $scope.$watch('badgeId', function () {
             $scope.error = null;
         });
-
         $scope.login=(id)=>{
-            console.log(id)
+            $http.get('https://nashvillebarcamp.firebaseio.com/.json')
+            .then(console.log)
 
         }
 })
