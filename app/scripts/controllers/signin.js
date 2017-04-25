2893121f4293040676be59b3667e684a2feb921f
Dominic Serrano <dmsrojo@gmail.com>
2017-04-25 17:57:24 -0500

    Added error handling to login template

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -1,21 +1,21 @@
 'use strict';
 
-app.controller('SigninCtrl', function ($scope,$route, $location,AuthService) {
+app.controller('SigninCtrl', function ($scope, $location, AuthService) {
 
 	AuthService.getAllUsers().then((userList)=>{
 		$scope.badges = userList;
 		// console.log($scope.badges);
 	});
 
-	$scope.$watch('badgeId', function () {
-      $scope.error = null;
-  });
-
   $scope.login = (id) => {
+		if (!id) { return $scope.error = 'Please enter a badge ID.'};
+
   	Object.keys($scope.badges).forEach((badge)=>{
   		if ($scope.badgeId.toUpperCase() === badge){
   			$location.path('/#!/sessions');
-  		};
+  		} else {
+				$scope.error = 'Invalid badge ID.';
+			};
   	});
  	};
 
