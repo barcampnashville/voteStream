f3a819a9d9356a67c259282710045fd935b8b253
David Cramb <david.cramb@gmail.com>
2017-05-09 20:22:05 -0500

    Added error messaging and modal pop-ups for vote checkboxes and vote submit button

--- app/scripts/controllers/session-listing.js
+++ app/scripts/controllers/session-listing.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('SessionListingCtrl', function($scope, SessionListing) {
+app.controller('SessionListingCtrl', function($scope, SessionListing, $http) {
 	
 	//jquery to control session tabs
 	$('#myTabs a').click(function (e) {
@@ -22,20 +22,32 @@ app.controller('SessionListingCtrl', function($scope, SessionListing) {
   	//Need error handling for more than 3 votes
   	
 	// check to see if the array is empty
-	if ($scope.voteArray.length === 0){
-		$scope.voteArray.push(index)
-
-	// check to see if length is less than three AND voteArray does note include duplicate
-	} else if( $scope.voteArray.length < 3 && !$scope.voteArray.includes(index)) {
+		if ($scope.voteArray.length === 0){
+			$scope.voteArray.push(index)
+			// check to see if length is less than three AND voteArray does note include duplicate
+		} else if( $scope.voteArray.length < 3 && !$scope.voteArray.includes(index)) {
   		$scope.voteArray.push(index)
-  		console.log("voteArray: ", $scope.voteArray)
-  	
-  	//display error message
-  	} else {
-  		console.log("else statement")
-  	}
+		} else {
+  		$scope.errorMessage = "You may select only 3 sessions.";
+  		$("#error-modal").modal("show");
+		}
   }
 
+  $scope.voteSubmit = () => {
+  	if($scope.voteArray.length < 3) {
+  		$scope.errorMessage = "Please vote for 3 sessions before submitting!";
+  	} else {
+
+  	//****We don't have access to firebase to post yet****
+  		
+  		// $http.post('https://nashvillebarcamp.firebaseio.com/Users/1FY13NK8/sessions.json', $scope.voteArray)
+  		// .then(function(response){
+  		// 	console.log(response)
+  		// })
+  		//submit to Firebase
+
+  	}
+  }
   //returns all sessions from sessions.js in services
 	SessionListing.getAllSessions().
 	then(sessionList => {
