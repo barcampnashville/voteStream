e79ac9d94a4b06e55def5e23b2a6af302dc0fb7e
David Cramb <david.cramb@gmail.com>
2017-07-23 22:06:28 -0500

    Sorting somewhat works on admin page. Not working - Rank does not follow on sort and Speaker is not sorting right. Up-Caret is not displaying properly.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,9 +1,14 @@
 'use strict';
 
-app.controller('AdminCtrl', function ($scope, SessionList) {
+app.controller('AdminCtrl', function ($scope, SessionList, Polling) {
 
-	$scope.sessions = SessionList;
+
+  $scope.sessions = SessionList;
+  $scope.availability = Polling;
+  $scope.sortByType = "rank";
+  $scope.reverseSort = false;
+
 	console.log("$scope.sessions-->", $scope.sessions); 
-
+  console.log("$scope.availability -->", $scope.availability);
 
 });
