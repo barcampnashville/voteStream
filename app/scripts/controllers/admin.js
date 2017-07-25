5edb05e92e0509451d2b7513bf3c8290bccc503b
David Cramb <david.cramb@gmail.com>
2017-07-25 18:55:24 -0500

    Added ranking sorting functionality. Sort by name still fubar

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,14 +1,40 @@
 'use strict';
 
-app.controller('AdminCtrl', function ($scope, SessionList, Polling) {
+app.controller('AdminCtrl', function ($scope, $filter, SessionList, Polling) {
+  $scope.unSortedSessions = SessionList;
+  $scope.sessions = [];
+  //filter sessions by total_votes
+  $scope.addSessionRankingByVotes = () => {
+    $scope.SessionListings = $filter('orderBy')($scope.unSortedSessions, 'total_votes', !$scope.reverse);
+    let i = 0;
+      angular.forEach($scope.SessionListings, function(value, key){
+        $scope.SessionListings[i].Rank = i+1;
+        i++;
+      });
+    $scope.sessions = $scope.SessionListings;
+    console.log($scope.sessions)
+
+  }
+  $scope.addSessionRankingByVotes();
+  // console.log($scope.sessions)
+
+  
 
 
-  $scope.sessions = SessionList;
+  // $scope.addStackRank = () => {
+    
+      
+
+  //   })
+  // }
+  // $scope.addStackRank();
+
+
   $scope.availability = Polling;
   $scope.sortByType = "rank";
   $scope.reverseSort = false;
 
-	console.log("$scope.sessions-->", $scope.sessions); 
+	// console.log("$scope.sessions-->", $scope.sessions); 
   console.log("$scope.availability -->", $scope.availability);
 
 });
