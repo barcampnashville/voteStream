88330ff9515fcdb6569b4e5756efd63a9b6642a7
David Cramb <david.cramb@gmail.com>
2017-09-05 18:55:16 -0500

    laying out basic framework for full schedule template page and controller. Controller pulls in Schedule from firebase

--- app/scripts/controllers/fullschedule.js
+++ app/scripts/controllers/fullschedule.js
@@ -1,11 +1,22 @@
 'use strict'
-
 app.controller('FullScheduleCtrl', function ($scope) {
 
   firebase.database().ref('/Sessions').on('value', function(session) {
-    $scope.fullSchedule = session.val();
+    $scope.allSessions = session.val();
+    // console.log($scope.fullSchedule);
     $scope.$apply();
   });
+  firebase.database().ref('/Schedules').on('value', function(schedule){
+    $scope.fullSchedule = schedule.val();
+    console.log($scope.fullSchedule);
+    $scope.$apply();
+  })
+  $('#myTabs a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+
 
 });
 
