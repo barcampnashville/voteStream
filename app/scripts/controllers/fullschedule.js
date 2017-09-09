0e5e869db7b4cf0987707010e635a8ea4b40625b
David Cramb <david.cramb@gmail.com>
2017-09-09 10:38:05 -0500

    creating array with rooms and times for sessions

--- app/scripts/controllers/fullschedule.js
+++ app/scripts/controllers/fullschedule.js
@@ -1,23 +1,53 @@
 'use strict'
 app.controller('FullScheduleCtrl', function ($scope) {
+
+  //change from jQuery
   $('#myTabs a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
   });
-  $scope.fullSchedule = "";
+
+  $scope.scheduleByTime = [];
+  $scope.rooms = [];
+  $scope.sessionsByTime = [];
 
   firebase.database().ref('/Sessions').on('value', function(session) {
     $scope.allSessions = session.val();
-    // console.log($scope.fullSchedule);
     $scope.$apply();
   });
   firebase.database().ref('/Schedules').on('value', function(schedule){
     $scope.fullSchedule = schedule.val();
     console.log($scope.fullSchedule);
     $scope.$apply();
+    $scope.sortScheduleByTime();
   });
 
-  
+  $scope.sortScheduleByTime = () => {
+
+    let scheduleObj = $scope.fullSchedule.Morning.rooms;
+    let numberOfRooms = Object.keys(scheduleObj).length;
+    for (let i = 0; i <= numberOfRooms -1; i++) {
+      $scope.rooms.push(scheduleObj[i])
+      for (let j = 0; j < scheduleObj[i].times.length; j++){
+        $scope.rooms[i].times[j]["room"] = $scope.rooms[i].name;
+      }
+      $scope.sessionsByTime["times"] = Object.values(scheduleObj[i].times);
+      //take name add it to each object in times array and add it to each object it times array and push each of those into scope array
+
+    }
+
+    console.log($scope.sessionsByTime)
+    console.log($scope.rooms)
+
+
+      
+    
+
+
+  }
+    
+
+
 
 
 
