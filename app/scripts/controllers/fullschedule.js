ecfcf27e179d91810fd946747ae87970d6ccfe3b
David Cramb <david.cramb@gmail.com>
2017-09-09 11:06:53 -0500

    created array with rooms and times by session for data filtering

--- app/scripts/controllers/fullschedule.js
+++ app/scripts/controllers/fullschedule.js
@@ -9,47 +9,31 @@ app.controller('FullScheduleCtrl', function ($scope) {
 
   $scope.scheduleByTime = [];
   $scope.rooms = [];
-  $scope.sessionsByTime = [];
-
-  firebase.database().ref('/Sessions').on('value', function(session) {
-    $scope.allSessions = session.val();
-    $scope.$apply();
-  });
   firebase.database().ref('/Schedules').on('value', function(schedule){
-    $scope.fullSchedule = schedule.val();
-    console.log($scope.fullSchedule);
     $scope.$apply();
-    $scope.sortScheduleByTime();
-  });
-
-  $scope.sortScheduleByTime = () => {
-
-    let scheduleObj = $scope.fullSchedule.Morning.rooms;
-    let numberOfRooms = Object.keys(scheduleObj).length;
-    for (let i = 0; i <= numberOfRooms -1; i++) {
-      $scope.rooms.push(scheduleObj[i])
-      for (let j = 0; j < scheduleObj[i].times.length; j++){
-        $scope.rooms[i].times[j]["room"] = $scope.rooms[i].name;
-      }
-      $scope.sessionsByTime["times"] = Object.values(scheduleObj[i].times);
-      //take name add it to each object in times array and add it to each object it times array and push each of those into scope array
-
+    //morning or afternoon switch
+    if (schedule.val().Morning){
+    $scope.sortScheduleByTime(schedule.val(), "Morning");
+    }
+    if (schedule.val().Afternoon){
+      $scope.sortScheduleByTime(schedule.val(), "Afternoon");
     }
 
-    console.log($scope.sessionsByTime)
+  });
+
+  $scope.sortScheduleByTime = (schedule, session) => {
+
+    let scheduleObj = schedule[session].rooms;
+    let numberOfRooms = Object.keys(scheduleObj).length;
+    for (let i = 0; i <= numberOfRooms -1; i++) {
+      for (let j = 0; j < scheduleObj[i].times.length; j++){
+        $scope.rooms.push(scheduleObj[i].times[j])
+        scheduleObj[i].times[j]["room"] = scheduleObj[i].name;
+      }
+    }
     console.log($scope.rooms)
-
-
-      
-    
-
-
   }
     
-
-
-
-
 
 });
 
