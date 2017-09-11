33ef81f718c6928bc99cfab2fd086d1049594435
David Cramb <david.cramb@gmail.com>
2017-09-10 21:28:19 -0500

    schedule page now hides afternoon schedule section if no afternoon schedule has been set from the admin tab via a boolean value in the schedule object. Schedule. SCH-ED-ULE

--- app/scripts/controllers/fullschedule.js
+++ app/scripts/controllers/fullschedule.js
@@ -1,16 +1,13 @@
 'use strict'
 app.controller('FullScheduleCtrl', function ($scope) {
 
-  //change from jQuery
- 
-
   $scope.scheduleByTime = [];
   $scope.rooms = [];
   $scope.filterBy = "time";
-  $scope.morning = false;
-  $scope.afternoon = false;
   $scope.morningRooms = [];
   $scope.afternoonRooms = [];
+  $scope.Afternoon = false;
+  $scope.Morning = false;
 
   firebase.database().ref('/Schedules').on('value', function(schedule){
     $scope.rooms = [];
@@ -18,21 +15,18 @@ app.controller('FullScheduleCtrl', function ($scope) {
     if (schedule.val().Morning){
       $scope.morningRooms = [];
       $scope.sortScheduleByTime(schedule.val(), "Morning");
-      $scope.morning = true;
+      checkIfScheduleExists(schedule.val().Morning, "Morning");
     }
     if (schedule.val().Afternoon){
       console.log(schedule.val().Afternoon)
       $scope.afternoonRooms = [];
       $scope.sortScheduleByTime(schedule.val(), "Afternoon");
-      $scope.afternoon = true;
+      checkIfScheduleExists(schedule.val().Afternoon, "Afternoon");
     }
     $scope.$apply();
-
   });
 
-
   $scope.sortScheduleByTime = (schedule, session) => {
-
     let scheduleObj = schedule[session].rooms;
     let numberOfRooms = Object.keys(scheduleObj).length;
     for (let i = 0; i <= numberOfRooms -1; i++) {
@@ -47,12 +41,16 @@ app.controller('FullScheduleCtrl', function ($scope) {
         }
       }
     }
-
-
   }
   
   $scope.setFilterString = (filterBy) => {
     $scope.filterBy = filterBy;
+  }
+
+  let checkIfScheduleExists = (schedule, timeOfDay) => {
+    if(schedule[timeOfDay] === true){
+      $scope[timeOfDay] = true;
+    }
   }
 
 });
